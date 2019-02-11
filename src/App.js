b44d86d0826564230dda3e3b787d765455f704b1
cristin <cristin.herrera.guerra@gmail.com>
2019-02-11 17:18:25 -0500

    implementando rutas

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ export class App extends Component {
     return (
       <div>
         <nav>
-          {/*Aca deben ir los links de navegacion*/}
+        <Link to="page1">Pagina 1</Link>
+        <Link to="page2">Pagina 2</Link>
         </nav>
-        {/* Aca tienes que agreager algo para que las rutas funcionen*/}
+        { this.props.children }
       </div>
     )
   }
